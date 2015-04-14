9cbf529fe9714a6660719747415994ddaeec0b91
luis-enrique <luis.enrique.mt@outlook.com>
2015-04-14 00:48:56 -0500

    luis

--- pages/index.php
+++ pages/index.php
@@ -1,72 +1,45 @@
-<?PHP 
-    session_start ();
-
-    if(!empty($_SESSION['session'])){
-        header('Location: usuario.php');
-    }else{
-        session_destroy ();
-    }
-?>
 
 <?php
 
     // inclulle la conexion a la bd del archivo "conexion.php"
     include("conexion.php");
+    
 
     // Start Compara si contiene algundato la variable global $_POST["usuario"]
-    if(!empty($_POST["usuario"])){
-        echo "con susuario"; //  QUITAR ESTO  
+    if(!empty($_POST["usuario"])){  
+        echo "con usuario"; //  QUITAR ESTO 
         
-        // Resibe los datos del formulario y los almacena en varibles
-        $usuario = $_POST["usuario"];
-        $pass = $_POST["password"];
+        $v_user =  $_POST['usuario'];
+        $v_pass = $_POST['password'];
         
-        // Se almasena un query en un avariable
-        $v_query = "SELECT * FROM usuarios WHERE usuario='".$usuario."'";
-
-        // Se ejecuta un Query tomando en cuelta la consulta lamasenada en la variable $v_query
+        $v_query = "SELECT u.id_usuario, u.usuario, u.contrasena, 
+                           t.nombre AS 't_nombre', t.apellido_p, t.apellido_m, t.e_mail, 
+                           tu.nombre AS 'tu_nombre', tu.privilegios, u.foto_perfil
+                    FROM usuarios u, trabajadores t, tipo_usuario tu
+                    WHERE u.id_trabajador = t.id_trabajador AND u.id_tipo_usuario = tu.id_tipo_usuario AND u.usuario ='".$v_user."'";
+        
         $registro = mysqli_query($link,$v_query) or die("Problemas en el select:".mysql_error());
         
-        // Comprueba y asosia la consulta en un Array
         if($reg = mysqli_fetch_array($registro, MYSQLI_ASSOC)){
-            // Compara el usuario y la contraseña con la base de datos
-            if($reg["password"] == SHA1("$pass")){
-               // Almacena los datos del array en variables globales e inicia seson el usuario
-                session_start ();
-                $_SESSION['session'] = "activo";
-                $_SESSION["usuario"] = $reg['usuario']; 
-                $_SESSION["Nombre"]  = $reg['nombre']." ".$reg['apellido_p']." ".$reg['apellido_m']; 
-                $_SESSION["id_tipo_usuario"]  = $reg['id_tipo_usuario'];
-                $_SESSION["correo"]  = $reg['correo'];                
-                $_SESSION["foto_perfil"] = $reg['foto_perfil']; 
-                // ESTA ES LA PAGINA A DONDE NOS MANDARA SI TODO ES CORRECTO RETORNA EL USUARIO Y EL URL DE SU FOTO DE PERFIL
-                header('Location: sistema.php');                     
-            }else{
-                //  SI LA CONTRASEÑA ES INCORRECTA RETORNA LA URL DE SU FOTO DE PERFIL DEL USUARIO
-                $v_imge_perfil = $reg['foto_perfil'];
-                $v_error="La contrasena es incorrecta";
-                echo "--- Contraseña incorrecta | el usuario: ".$usuario." existe"; //  QUITAR ESTO 
-                
-            }
-                
-        }else{
-            //$v_errorusuario = $reg['usuario'];
-            //ESTA ES LA PAGINA A DONDE NOS MANDARA SI TODO ESTA MAL
-            // RETORNA EL USUARIO
-            // header('Location: usuario.php'); 
-            echo "--- Todo mal | el usuario ".$usuario." no existe"; //  QUITAR ESTO 
-            $v_imge_perfil = "imagenes/login.png";
-            $v_error="El usuari no existe";                
+            
+            echo $reg['id_usuario'];
+            echo $reg['usuario'];
+            echo $reg['contrasena'];
+            echo $reg['t_nombre']." ".$reg['apellido_p']." ".$reg['apellido_m'];
+            echo $reg['e_mail'];            
+            echo $reg['tu_nombre'];
+            echo $reg['privilegios'];
+            echo $reg['foto_perfil'];
         }
-
+        
+        
+        
+        
         
     }else{
         echo "sin usuario"; //  QUITAR ESTO 
-        $v_imge_perfil = "imagenes/login.png";
     }
-    // End Compara si contiene algundato la variable global $_POST["usuario"]
 ?>   
-
 
 
 <!DOCTYPE html>
